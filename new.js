00fbcc96fae775a1c0c36c66cf1b347f632929b4
omi <yxy@example.com>
2024-08-14 17:37:45 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,7 @@
+// This is a basic JavaScript function that logs a message to the console
+function logMessage() {
+    console.log("Hello! This is a message from the script.js file.");
+}
+
+// Call the logMessage function
+logMessage();
