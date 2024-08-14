fd1e1ab136a4f4bbc720c54c1bdaa4ca67e9f9bd
omi <yxy@example.com>
2024-08-14 20:25:31 +0530

    second command

--- new.js
+++ new.js
@@ -1,6 +1,6 @@
 // This is a basic JavaScript function that logs a message to the console
 function logMessage() {
-    console.log("Hello! This is a message from the script.js file.");
+    console.log("Hello! hhhhhhhhhhhhhhhhhhhhhhhhhhh.");
 }
 
 // Call the logMessage function
